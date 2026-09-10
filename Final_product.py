9b15ee3adf272ba9db16dbebf72031ea10a9bbe9
lsean8083-ux <lsean8083@gmail.com>
2026-09-10 22:18:05 +0700

    final product of trigger

---
+++ Final_product.py
@@ -0,0 +1,8 @@
+from GUI import VocabularyGUI
+from database import VocabularyDatabase
+
+if __name__ == "__main__":
+    database = VocabularyDatabase()
+    try: database.migrate_trial_2()  # Never modifies the Trial_2 database.
+    except RuntimeError: pass
+    VocabularyGUI().mainloop()
